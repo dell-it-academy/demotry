a262acb837edeed449aa15b7548bed20de247bde
HCley <coliveira@cipucrs.pucrs.br>
2019-08-09 11:23:24 -0300

    Commit folders

---
+++ src/main/java/com/try/demotry/DemotryApplication.java
@@ -0,0 +1,13 @@
+package com.try.demotry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/try/demotry/DemotryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.try.demotry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemotryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
